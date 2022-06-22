56b125c0d411ba86915add65ed18e66acfb05c47
pakkawee <pakkawee.1@gmail.com>
2022-06-22 09:42:29 +0700

    pakkawee

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+ import {View, Text} from 'react-native';
+
+ const styles StyleSheet.create({
+
+ })
+
+ const App = () => {
+   return(
+     <View>
+       <Text> Bhunnathon Treelertsakul </Text>
+       <Text>Engineer</Text>
+       <Text>Thai-Nichi Institude of Technology</Text>
+     </View>
+   );
+ }
+
+ export default App;
